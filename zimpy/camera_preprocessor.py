a737a39815e81af4f6a3c46f3716375ba6bd1ce8
Matthew Zimmer <matthewz@channeleyes.com>
2016-12-13 04:45:41 -0500

    chopping top of image off first. the idea is that the network sees only around 2-3 meters in front itself thus converging more to inseen roads.

--- zimpy/camera_preprocessor.py
+++ zimpy/camera_preprocessor.py
@@ -8,10 +8,10 @@ def flip_image(image_array, steering_angle):
 
 def preprocess_image(image_array, output_shape=(160, 320)):
     # hard-code this so drive.py and training_test.py use same size (refactor later)
-    output_shape = (66, 200)
+    # output_shape = (66, 200)
     # output_shape = (16, 32)
     # output_shape = (32, 64)
-    # output_shape = (160, 320)
+    output_shape = (160, 320)
 
     # 1. Resize/normalize to desired shape
     image_array = cv2.resize(image_array, (output_shape[1], output_shape[0]), interpolation=cv2.INTER_AREA)
@@ -20,10 +20,10 @@ def preprocess_image(image_array, output_shape=(160, 320)):
     # image_array = cv2.normalize(image_array, image_array, norm_type=cv2.NORM_MINMAX)
 
     # 2. crop top third of image
-    # h, w = image_array.shape[0:2]
-    # y1 = int(h/3)
-    # y2 = h
-    # image_array = image_array[y1:y2, 0:w]
+    h, w = image_array.shape[0:2]
+    y1 = int(h/2)
+    y2 = h
+    image_array = image_array[y1:y2, 0:w]
 
     return image_array
 
