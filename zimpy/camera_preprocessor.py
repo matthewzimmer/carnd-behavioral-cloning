f3e447cb972c04036153ad5f7db3a79962699015
Matthew Zimmer <M16r01@gmail.com>
2016-12-14 02:07:26 -0500

    clean up

--- zimpy/camera_preprocessor.py
+++ zimpy/camera_preprocessor.py
@@ -8,22 +8,22 @@ def flip_image(image_array, steering_angle):
 
 def preprocess_image(image_array, output_shape=(160, 320)):
     # hard-code this so drive.py and training_test.py use same size (refactor later)
-    # output_shape = (66, 200)
+    output_shape = (66, 200)
     # output_shape = (16, 32)
     # output_shape = (32, 64)
-    output_shape = (160, 320)
+    # output_shape = (160, 320)
+
+    # crop top third of image
+    h, w = image_array.shape[0:2]
+    y1 = h/20 #int(h/2)
+    y2 = h
+    image_array = image_array[y1:y2, 0:w]
 
     # 1. Resize/normalize to desired shape
     image_array = cv2.resize(image_array, (output_shape[1], output_shape[0]), interpolation=cv2.INTER_AREA)
     # image_array = cv2.resize(image_array, (output_shape[1], output_shape[0]), interpolation=cv2.INTER_AREA) / 255.0
     # image_array = cv2.resize(image_array, (output_shape[1], output_shape[0]), interpolation=cv2.INTER_AREA) / 127.5 - 1
     # image_array = cv2.normalize(image_array, image_array, norm_type=cv2.NORM_MINMAX)
-
-    # 2. crop top third of image
-    h, w = image_array.shape[0:2]
-    y1 = int(h/2)
-    y2 = h
-    image_array = image_array[y1:y2, 0:w]
 
     return image_array
 
