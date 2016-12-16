7a090ff77ce49c333a2d96009e945bbdc0cec203
Matthew Zimmer <M16r01@gmail.com>
2016-12-15 22:06:21 -0500

    Nvidia2 impl.

--- training.py
+++ training.py
@@ -333,6 +333,47 @@ class Nvidia(BaseNetwork):
         return self.model
 
 
+class Nvidia2(BaseNetwork):
+    def get_model(self, input_shape, output_shape, learning_rate=0.0001, use_weights=True):
+        model = None
+
+        optimizer = Adam(lr=learning_rate)
+        loss = 'msle'
+        if use_weights:
+            model = self.restore(optimizer=optimizer, loss=loss)
+        if model is None:
+            model = Sequential()
+            model.add(Lambda(lambda x: x / 127.5 - 1.,
+                             input_shape=input_shape,
+                             output_shape=output_shape))
+            model.add(Dropout(.2))
+            model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="valid"))
+            model.add(ELU())
+            model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode="valid"))
+            model.add(ELU())
+            model.add(Convolution2D(48, 5, 5, subsample=(2, 2), border_mode="valid"))
+            model.add(ELU())
+            model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode="valid"))
+            model.add(ELU())
+            model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode="valid"))
+            model.add(Flatten())
+            model.add(ELU())
+            model.add(Dense(1164))
+            model.add(Dropout(.2))
+            model.add(Dense(100))
+            model.add(Dropout(.2))
+            model.add(Dense(50))
+            model.add(Dropout(.2))
+            model.add(ELU())
+            model.add(Dense(1))
+            model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
+        model.summary()
+        self.model = model
+
+        return self.model
+
+
 class Udacity(BaseNetwork):
     def get_model(self, input_shape=None, output_shape=None, learning_rate=0.0001):
         self.model = self.restore(optimizer=Adam(lr=learning_rate), loss='mse')
