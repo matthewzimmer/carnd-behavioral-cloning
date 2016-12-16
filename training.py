87b97b3e171536b977fbfe39a8467574c8a1cc54
matthewzimmer <M16r01@gmail.com>
2016-12-16 04:22:16 +0000

    WIP

--- training.py
+++ training.py
@@ -299,7 +299,7 @@ class Nvidia(BaseNetwork):
         model = None
 
         optimizer = Adam(lr=learning_rate)
-        loss = 'msle'
+        loss = 'mse'
         if use_weights:
             model = self.restore(optimizer=optimizer, loss=loss)
         if model is None:
