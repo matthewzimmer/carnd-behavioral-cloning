54b8d48310df830a37fb7cb0a90b5b5ee7236ed9
Matthew Zimmer <M16r01@gmail.com>
2016-12-20 00:59:08 -0500

    Ensuring I don't lose the model which drives car around track infinitely.

--- drive_basic.py
+++ drive_basic.py
@@ -23,8 +23,8 @@ MAX_THROTTLE = 0.25
 
 
 def get_throttle(steering_angle):
-    # return 0.2
-    return (-(MAX_THROTTLE - MIN_THROTTLE) * abs(steering_angle)) * 3 + MAX_THROTTLE
+    return 0.2
+    # return (-(MAX_THROTTLE - MIN_THROTTLE) * abs(steering_angle)) * 3 + MAX_THROTTLE
 
 
 @sio.on('telemetry')
