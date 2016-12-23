9902aa46d61f3689a769ac53d44619e7dad94ecb
Matthew Zimmer <M16r01@gmail.com>
2016-12-23 16:05:49 -0500

    hooked up code to execute main method.

--- basic_recovery_training.py
+++ basic_recovery_training.py
@@ -12,13 +12,19 @@ flags.DEFINE_float('lr', 0.0001, "Optimizer learning rate.")
 flags.DEFINE_float('dropout_prob', 0., "Percentage of neurons to misfire during training.")
 flags.DEFINE_string('activation', 'relu', "The activation function used by the network.")
 
-train_network(
-    output_shape=(40, 80, 3),
-    nb_epoch=FLAGS.epochs,
-    batch_size=FLAGS.batch_size,
-    validation_split_percentage=FLAGS.validation_split_percentage,
-    learning_rate=FLAGS.lr,
-    dropout_prob=FLAGS.dropout_prob,
-    activation=FLAGS.activation,
-    use_weighs=FLAGS.use_weights
-)
+
+def main(_):
+    train_network(
+        output_shape=(40, 80, 3),
+        nb_epoch=FLAGS.epochs,
+        batch_size=FLAGS.batch_size,
+        validation_split_percentage=FLAGS.validation_split_percentage,
+        learning_rate=FLAGS.lr,
+        dropout_prob=FLAGS.dropout_prob,
+        activation=FLAGS.activation,
+        use_weighs=FLAGS.use_weights
+    )
+
+# parses flags and calls the `main` function above
+if __name__ == '__main__':
+    tf.app.run()
